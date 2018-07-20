ab8bd0d37506d2afdfb1f2df5d57364e8562051c
guodongbo <guodongbo@tbkt.cn>
2018-07-20 11:28:26 +0800

    完善游戏无解问题

--- firstVueDemo/src/components/game-pt.vue
+++ firstVueDemo/src/components/game-pt.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <p class="update-info">敬请期待</p>
+    <p class="update-info">提示：改项目已上传到<a href="https://gitee.com/guodongbo/simple_vue_project" target="_black">https://gitee.com/guodongbo/simple_vue_project</a>欢迎下载查看</p>
   <div class="game">
-    <p class="game-title">拼图</p>
+    <p class="game-title">数字拼图</p>
     <div class="game-left">
       <div class="box">
           <ul class="puzzle-wrap">
@@ -15,7 +15,10 @@
     </div>
     <div class="game-right">
       <div class="ruler">
-        <p style="font-size: 1rem;color: red" v-if="pass">恭喜你,闯关成功!</p>
+        <p style="font-size: 1rem;color: red" >通过样图</p>
+        <img src="../assets/success.png" alt="" style="width: 120px;height: 120px">
+        <p style="font-size: 1rem;color: red" >步数：<span style="font-size: 2rem;color: blue">{{step}}</span></p>
+        <div style="font-size: 1rem;color: red;height: 50px;width: 200px" v-if="pass">恭喜你,闯关成功!</div>
       </div>
       <button class="jx" @click="begain1">通关</button>
       <button class="jx" @click="begain">重置游戏</button>
@@ -30,6 +33,7 @@ export default {
   name: 'game-pt',
   data () {
     return {
+      step: 0,
       puzzles: [],
       pass: false,
       list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ''] // 答案刘表
@@ -45,12 +49,8 @@ export default {
         puzzleArr.push(i)
       }
       // 随机打乱数组
-      puzzleArr = puzzleArr.sort(() => {
-        return Math.random() - 0.5
-      })
-      // 页面显示
-      this.puzzles = puzzleArr
-      this.puzzles.push('')
+      this.findReverseSize(puzzleArr)
+      this.step = 0
     },
     begain () {
       this.render()
@@ -76,29 +76,57 @@ export default {
       if (leftNum === '' && index % 4) {
         this.$set(this.puzzles, index - 1, curNum)
         this.$set(this.puzzles, index, '')
+        this.step++
       } else if (rightNum === '' && index % 4 !== 3) {
         this.$set(this.puzzles, index + 1, curNum)
         this.$set(this.puzzles, index, '')
+        this.step++
       } else if (topNum === '') {
         this.$set(this.puzzles, index - 4, curNum)
         this.$set(this.puzzles, index, '')
+        this.step++
       } else if (bottomNum === '') {
         this.$set(this.puzzles, index + 4, curNum)
         this.$set(this.puzzles, index, '')
+        this.step++
       }
       this.passFn()
     },
     // 校验是否过关
     passFn () {
       if (this.puzzles[15] === '') {
-        const newPuzzles = this.puzzles.slice(0, 15)
-        const isPass = newPuzzles.every((e, i) => e === i + 1)
+        const newPuzzles = this.puzzles.slice(0, 15) // 截取前15个元素生出一个新数组
+        const isPass = newPuzzles.every((e, i) => e === i + 1) // 判断当前值是否为其索引加1
         if (isPass) {
           this.pass = true
           this.$alert('恭喜你通关成功!', {center: true, type: 'warning'})
         }
       }
+    },
+    // 检查是否有解
+    findReverseSize (puzzleArr) {
+      var arr = puzzleArr.sort(() => {
+        return Math.random() - 0.5
+      })
+      let len = arr.length
+      let i
+      let reverseSize = 0
+      for (i = 0; i < len - 1; i++) {
+        let j
+        for (j = i; j < len; j++) {
+          if (arr[i] > arr[j]) {
+            reverseSize++
+          }
+        }
+      }
+      if (!(reverseSize % 2)) {
+        arr.push('')
+        this.puzzles = arr
+      } else {
+        this.findReverseSize(this.list.filter((val) => val)) // 去掉列表最后的一个空值
+      }
     }
+
   },
   mounted () {
     this.render()
@@ -125,8 +153,8 @@ export default {
     float: left;
     width: 100px;
     height: 100px;
-    font-size: 20px;
-    background: #f90;
+    font-size: 30px;
+    background: lightskyblue;
     text-align: center;
     line-height: 100px;
     border: 1px solid #ccc;
@@ -187,7 +215,7 @@ export default {
     color: black;
   }
   .ruler{
-    height: 300px;
+    height: 280px;
     padding-top: 30px;
   }
 </style>
